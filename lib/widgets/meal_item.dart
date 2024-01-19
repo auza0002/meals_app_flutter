d2db6499c4d7c84a48720e45b17df5164f93123f
Diego Auza <diegoauza@Diegos-MacBook-Pro.local>
2024-01-19 10:35:03 -0500

    adding transparent image

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app_flutter/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: InkWell(
+      onTap: (){},
+      child: ,
+    ),);
+  }
+}
