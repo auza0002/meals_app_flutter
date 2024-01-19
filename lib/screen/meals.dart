126f968f8fc5e674c9a3987b3a76b598d5e9a60f
Diego Auza <diegoauza@Diegos-MacBook-Pro.local>
2024-01-19 11:07:36 -0500

    meal item traid working

--- lib/screen/meals.dart
+++ lib/screen/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app_flutter/models/meal.dart';
+import 'package:meals_app_flutter/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.meals, required this.title});
@@ -10,14 +11,8 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
-      itemCount: meals.length,
-      itemBuilder: (ctx, index) => Text(
-        meals[index].title,
-        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
-      ),
-    );
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]));
 
     if (meals.isEmpty) {
       content = Center(
